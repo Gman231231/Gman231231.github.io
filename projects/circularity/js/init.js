fcebf7e8875791a85c075ae8048aea2cdc698772
Gman231231 <gunnar.smith@jpschools.org>
2024-11-23 06:15:49 +0000

    refractor to create 100 circles using loops

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,14 +20,23 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
-
+        var circle;
+        var circles = [];
 
         // TODO 2 : Create a function that draws a circle 
-        
+        function drawCircle(){
+    circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
+    physikz.addRandomVelocity(circle, canvas);
+    view.addChild(circle);
+    circles.push(circle);
+        }
 
         // TODO 3 / 7 : Call the drawCircle() function 
-
-
+        var loopsCompleted = 0;
+        while (loopsCompleted < 100) {
+       drawCircle();
+        loopsCompleted++;
+        }
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
         ////////////////////////////////////////////////////////////
@@ -39,11 +48,12 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-
-            
+            for (var i = 0; i < circles.length; i++) {
+                physikz.updatePosition(circles[i])   
+                game.checkCirclePosition(circles[i])         
+              }
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-           
-
+            
             // TODO 9 : Iterate over the array
            
             
@@ -63,7 +73,6 @@ var init = function (window) {
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
             
-
 
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
         }
